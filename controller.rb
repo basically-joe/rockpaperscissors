e86f84782e0e771eab8c3da0a869b8691b7db324
Joe McColl <joe4kool@gmail.com>
2018-12-11 20:22:17 +0000

    created controller that comes back with expected strongs within browser

---
+++ controller.rb
@@ -0,0 +1,9 @@
+require("sinatra")
+require("sinatra/contrib/all")
+require("pry-byebug")
+require_relative("models/rps_model")
+also_reload("./models/*")
+
+get "/rps/:hand1/:hand2" do
+  RPSGame.check_win(params[:hand1], params[:hand2])
+end
